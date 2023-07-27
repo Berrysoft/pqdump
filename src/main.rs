27daa86773849ee5f2f952c5a3d0daa753739ce8
Berrysoft <Strawberry_Str@hotmail.com>
2023-07-27 15:30:26 +0800

    Add more types support.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use anyhow::Result;
-use arrow_array::{
-    cast::AsArray,
-    types::{Int32Type, Int64Type},
-    Array, Int32Array, Int64Array, RecordBatchReader,
-};
+use arrow_array::{cast::AsArray, types::*, *};
 use arrow_schema::{DataType, Field};
 use clap::Parser;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
@@ -37,8 +33,7 @@ impl From<&Field> for PrintedField {
     }
 }
 
-trait ToStringArray {
-    fn get_len(&self) -> usize;
+trait ToStringArray: Array {
     fn get_string(&self, i: usize) -> String;
 }
 
@@ -46,10 +41,6 @@ macro_rules! impl_to_string_array {
     ($($t: ty),* $(,)?) => {
         $(
             impl ToStringArray for $t {
-                fn get_len(&self) -> usize {
-                    self.len()
-                }
-
                 fn get_string(&self, i: usize) -> String {
                     self.value(i).to_string()
                 }
@@ -58,19 +49,81 @@ macro_rules! impl_to_string_array {
     };
 }
 
-impl_to_string_array!(Int32Array, Int64Array);
+impl_to_string_array!(
+    BooleanArray,
+    Float32Array,
+    Float64Array,
+    Int8Array,
+    Int16Array,
+    Int32Array,
+    Int64Array,
+    UInt8Array,
+    UInt16Array,
+    UInt32Array,
+    UInt64Array,
+);
+
+trait FromDynArray {
+    type ArrayType: Array;
+
+    fn from_dyn_array(arr: &dyn Array) -> &Self::ArrayType;
+}
+
+macro_rules! impl_from_array_primitive {
+    ($(($arrty: ty, $t: ty)),* $(,)?) => {
+        $(
+            impl FromDynArray for $t {
+                type ArrayType = $arrty;
+
+                fn from_dyn_array(arr: &dyn Array) -> &Self::ArrayType {
+                    arr.as_primitive::<$t>()
+                }
+            }
+        )*
+    };
+}
+
+impl_from_array_primitive!(
+    (Float32Array, Float32Type),
+    (Float64Array, Float64Type),
+    (Int8Array, Int8Type),
+    (Int16Array, Int16Type),
+    (Int32Array, Int32Type),
+    (Int64Array, Int64Type),
+    (UInt8Array, UInt8Type),
+    (UInt16Array, UInt16Type),
+    (UInt32Array, UInt32Type),
+    (UInt64Array, UInt64Type),
+);
+
+impl FromDynArray for BooleanType {
+    type ArrayType = BooleanArray;
+
+    fn from_dyn_array(arr: &dyn Array) -> &Self::ArrayType {
+        arr.as_boolean()
+    }
+}
 
 macro_rules! case_as_primitive {
-    ($arr: expr, $($id: ident, $t: ty),* $(,)?) => {
+    ($arr: expr, $($t: ty),* $(,)?) => {
         match $arr.data_type() {
-            $(arrow_schema::DataType::$id => $arr.as_primitive::<$t>(),)*
+            $(&<$t>::DATA_TYPE => <$t>::from_dyn_array($arr),)*
             _ => unimplemented!("Unsupported data type"),
         }
     };
 }
 
 fn transform_array(arr: &dyn Array) -> &dyn ToStringArray {
-    case_as_primitive!(arr, Int32, Int32Type, Int64, Int64Type)
+    case_as_primitive!(
+        arr,
+        BooleanType,
+        Float32Type,
+        Float64Type,
+        Int8Type,
+        Int16Type,
+        Int32Type,
+        Int64Type,
+    )
 }
 
 fn main() -> Result<()> {
@@ -98,7 +151,7 @@ fn main() -> Result<()> {
                 .map(|c| transform_array(c.as_ref()))
                 .collect::<Vec<_>>();
             if let Some(col) = columns.get(0) {
-                let len = col.get_len();
+                let len = col.len();
                 let mut builder = Builder::default();
                 for i in 0..len {
                     let row = columns
