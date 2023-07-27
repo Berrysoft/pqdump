1a0359ab2597b9978b490552073f70a062f052c1
Berrysoft <Strawberry_Str@hotmail.com>
2023-07-27 16:35:59 +0800

    Add length & slice commands.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,35 @@ use arrow_array::{cast::AsArray, types::*, *};
 use arrow_schema::{DataType, Field};
 use clap::Parser;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
-use std::{ffi::OsString, fs::File};
+use std::{collections::VecDeque, ffi::OsString, fs::File};
 use tabled::{builder::Builder, settings::Style, Table, Tabled};
+use tryiterator::TryIteratorExt;
 
 #[derive(Debug, Parser)]
 #[command(about, version, author)]
 struct Options {
     #[arg()]
     input: OsString,
+    #[arg(short = 'n', long)]
+    /// Print the number of rows and exit.
+    length: bool,
     #[arg(short = 'A', long)]
     /// Print the datatypes only.
     only_types: bool,
     #[arg(long)]
     /// Suppress printing the datatypes.
     no_types: bool,
+    #[command(flatten)]
+    slice: SliceOptions,
+}
+
+#[derive(Debug, Parser)]
+#[group(multiple = false)]
+struct SliceOptions {
+    #[arg(long)]
+    head: Option<usize>,
+    #[arg(long)]
+    tail: Option<usize>,
 }
 
 #[derive(Debug, Tabled)]
@@ -139,43 +154,81 @@ fn transform_array(arr: &dyn Array) -> &dyn ToStringArray {
 fn main() -> Result<()> {
     let args = Options::parse();
     let reader = ParquetRecordBatchReaderBuilder::try_new(File::open(args.input)?)?.build()?;
-    if !args.no_types {
-        let fields = reader
-            .schema()
-            .fields()
-            .iter()
-            .map(|f| PrintedField::from(f.as_ref()))
-            .collect::<Vec<_>>();
-        println!("{}", Table::new(fields).with(Style::rounded()));
-    }
-    if !args.only_types {
-        for batch in reader {
-            let batch = batch?;
-            let field_names = batch
+    if args.length {
+        let len = reader
+            .into_iter()
+            .map(|batch| batch.map(|batch| batch.num_rows()))
+            .try_fold(0, |sum, i| i.map(|i| sum + i))?;
+        println!("{}", len);
+    } else {
+        if !args.no_types {
+            let fields = reader
+                .schema()
+                .fields()
+                .iter()
+                .map(|f| PrintedField::from(f.as_ref()))
+                .collect::<Vec<_>>();
+            println!("{}", Table::new(fields).with(Style::rounded()));
+        }
+        if !args.only_types {
+            let field_names = reader
                 .schema()
                 .fields()
                 .iter()
                 .map(|f| f.name().clone())
                 .collect::<Vec<_>>();
-            let columns = batch
-                .columns()
-                .iter()
-                .map(|c| transform_array(c.as_ref()))
-                .collect::<Vec<_>>();
-            if let Some(col) = columns.get(0) {
-                let len = col.len();
-                let mut builder = Builder::default();
-                for i in 0..len {
-                    let row = columns
-                        .iter()
-                        .map(|col| col.get_string(i))
-                        .collect::<Vec<_>>();
-                    builder.push_record(row);
+            let rows = reader
+                .into_iter()
+                .map(|batch| {
+                    batch.map(|batch| {
+                        let columns = batch
+                            .columns()
+                            .iter()
+                            .map(|c| transform_array(c.as_ref()))
+                            .collect::<Vec<_>>();
+                        (0..batch.num_rows())
+                            .map(|i| {
+                                anyhow::Ok(
+                                    columns
+                                        .iter()
+                                        .map(|col| col.get_string(i))
+                                        .collect::<Vec<_>>(),
+                                )
+                            })
+                            .collect::<Vec<_>>()
+                            .into_iter()
+                    })
+                })
+                .try_flatten();
+            if let Some(head) = args.slice.head {
+                print_contents(field_names, rows.take(head))?;
+            } else if let Some(tail) = args.slice.tail {
+                let mut buf = VecDeque::new();
+                for row in rows {
+                    buf.push_back(row);
+                    if buf.len() > tail {
+                        buf.pop_front();
+                    }
                 }
-                builder.set_header(field_names);
-                println!("{}", builder.build().with(Style::rounded()));
+                print_contents(field_names, buf.into_iter())?;
+            } else {
+                print_contents(field_names, rows)?;
             }
         }
     }
+    Ok(())
+}
+
+fn print_contents<E>(
+    columns: Vec<String>,
+    rows: impl Iterator<Item = Result<Vec<String>, E>>,
+) -> Result<(), E> {
+    let mut builder = Builder::new();
+    for row in rows {
+        let row = row?;
+        builder.push_record(row);
+    }
+    builder.set_header(columns);
+    println!("{}", builder.build().with(Style::rounded()));
     Ok(())
 }
