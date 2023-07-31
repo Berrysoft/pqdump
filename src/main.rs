286fe5eaa283169bc95f683b0a5b3b1e039dcc8f
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-07-31 23:48:06 +0800

    Add --num-row-groups.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ struct Options {
     #[arg(short = 'n', long)]
     /// Print the number of rows and exit.
     length: bool,
+    #[arg(long)]
+    /// Print the number of row groups and exit.
+    num_row_groups: bool,
     #[arg(short = 'A', long)]
     /// Print the datatypes only.
     only_types: bool,
@@ -74,77 +77,83 @@ fn main() -> Result<()> {
     let args = Options::parse();
     let reader = ParquetRecordBatchReaderBuilder::try_new(File::open(args.input)?)?
         .with_batch_size(args.batch);
-    let len = reader.metadata().file_metadata().num_rows() as usize;
-    let reader = reader.build()?;
-    if args.length {
-        println!("{}", len);
+    let metadata = reader.metadata();
+    if args.num_row_groups {
+        println!("{}", metadata.num_row_groups());
     } else {
-        let schema = reader.schema();
-        if !args.no_types {
-            let fields = schema
-                .fields()
-                .iter()
-                .map(|f| PrintedField::from(f.as_ref()))
-                .collect::<Vec<_>>();
-            println!("{}", Table::new(fields).with(Style::rounded()));
-        }
-        if !args.only_types {
-            let field_names = schema.fields().iter().map(|f| f.name().clone());
-            let (field_indices, field_names): (Vec<_>, Vec<_>) =
-                if let Some(columns) = args.col.columns {
-                    field_names
-                        .enumerate()
-                        .filter(|(_, n)| columns.contains(n))
-                        .unzip()
-                } else if let Some(exclude) = args.col.exclude {
-                    field_names
-                        .enumerate()
-                        .filter(|(_, n)| !exclude.contains(n))
-                        .unzip()
+        let len = reader.metadata().file_metadata().num_rows() as usize;
+        let reader = reader.build()?;
+        if args.length {
+            println!("{}", len);
+        } else {
+            let schema = reader.schema();
+            if !args.no_types {
+                let fields = schema
+                    .fields()
+                    .iter()
+                    .map(|f| PrintedField::from(f.as_ref()))
+                    .collect::<Vec<_>>();
+                println!("{}", Table::new(fields).with(Style::rounded()));
+            }
+            if !args.only_types {
+                let field_names = schema.fields().iter().map(|f| f.name().clone());
+                let (field_indices, field_names): (Vec<_>, Vec<_>) =
+                    if let Some(columns) = args.col.columns {
+                        field_names
+                            .enumerate()
+                            .filter(|(_, n)| columns.contains(n))
+                            .unzip()
+                    } else if let Some(exclude) = args.col.exclude {
+                        field_names
+                            .enumerate()
+                            .filter(|(_, n)| !exclude.contains(n))
+                            .unzip()
+                    } else {
+                        field_names.enumerate().unzip()
+                    };
+                let (skip, take) = if let Some(head) = args.slice.head {
+                    (0, head.min(len))
+                } else if let Some(tail) = args.slice.tail {
+                    if len <= tail {
+                        (0, len)
+                    } else {
+                        (len - tail, tail)
+                    }
                 } else {
-                    field_names.enumerate().unzip()
-                };
-            let (skip, take) = if let Some(head) = args.slice.head {
-                (0, head.min(len))
-            } else if let Some(tail) = args.slice.tail {
-                if len <= tail {
                     (0, len)
+                };
+                let skip_batches = skip / args.batch;
+                let skip = skip % args.batch;
+                let take_batches = (skip + take) / args.batch;
+                let take_batches = if ((skip + take) % args.batch) != 0 {
+                    take_batches + 1
                 } else {
-                    (len - tail, tail)
-                }
-            } else {
-                (0, len)
-            };
-            let skip_batches = skip / args.batch;
-            let skip = skip % args.batch;
-            let take_batches = (skip + take) / args.batch;
-            let take_batches = if ((skip + take) % args.batch) != 0 {
-                take_batches + 1
-            } else {
-                take_batches
-            };
-            let batches = reader
-                .into_iter()
-                .skip(skip_batches)
-                .take(take_batches)
-                .try_collect::<Vec<_>>()?;
-            let columns = batches
-                .iter()
-                .map(|batch| {
-                    batch
-                        .columns()
-                        .iter()
-                        .enumerate()
-                        .filter(|(i, _)| field_indices.contains(i))
-                        .map(|(_, c)| ArrayFormatter::try_new(c, &FormatOptions::default()))
-                        .try_collect::<Vec<_>>()
-                        .map(|columns| (batch.num_rows(), columns))
-                })
-                .try_collect::<Vec<_>>()?;
-            let rows = columns.iter().flat_map(|(num_rows, columns)| {
-                (0..*num_rows).map(|i| columns.iter().map(move |col| col.value(i).try_to_string()))
-            });
-            print_contents(field_names, rows.skip(skip).take(take))?;
+                    take_batches
+                };
+                let batches = reader
+                    .into_iter()
+                    .skip(skip_batches)
+                    .take(take_batches)
+                    .try_collect::<Vec<_>>()?;
+                let columns = batches
+                    .iter()
+                    .map(|batch| {
+                        batch
+                            .columns()
+                            .iter()
+                            .enumerate()
+                            .filter(|(i, _)| field_indices.contains(i))
+                            .map(|(_, c)| ArrayFormatter::try_new(c, &FormatOptions::default()))
+                            .try_collect::<Vec<_>>()
+                            .map(|columns| (batch.num_rows(), columns))
+                    })
+                    .try_collect::<Vec<_>>()?;
+                let rows = columns.iter().flat_map(|(num_rows, columns)| {
+                    (0..*num_rows)
+                        .map(|i| columns.iter().map(move |col| col.value(i).try_to_string()))
+                });
+                print_contents(field_names, rows.skip(skip).take(take))?;
+            }
         }
     }
     Ok(())
